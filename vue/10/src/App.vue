c61cc10660dd86346e2f0e3da03a1223cc7f9e13
1096642300 <1096642300@qq.com>
2023-05-19 16:37:17 +0800

    vue-cli创建项目

---
+++ vue/10/src/App.vue
@@ -0,0 +1,28 @@
+<!--组件的模板结构-->
+<template>
+  <div>
+    <h2 @click="changeMsg">App.vue...{{ msg }}</h2>
+  </div>
+</template>
+
+<!--组件的js代码-->
+<script>
+// 以后在这里面写vue的配置内容
+export default {
+  data () {
+    return {
+      msg: 'hello'
+    }
+  },
+  methods: {
+    changeMsg () {
+      this.msg = 'world'
+    }
+  }
+}
+</script>
+
+<!--组件的css样式-->
+<style lang="scss">
+
+</style>
